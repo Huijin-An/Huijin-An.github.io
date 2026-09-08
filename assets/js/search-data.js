b280ee95306f8d7227c88a3245cfbb8c5f4a0599
Huijin-An <128879315+Huijin-An@users.noreply.github.com>
2026-09-08 03:02:03 +0000

    Deploying to gh-pages from @ Huijin-An/Huijin-An.github.io@5ab1d187cea4103aff083ef0df852015c7b40cb8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,14 +106,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Huijin_Resume_2026.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%75%69%6A%69%6E.%61%6E@%76%61%6E%64%65%72%62%69%6C%74.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
